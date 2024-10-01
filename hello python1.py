ea924f60f27cf32e76b619479c81d4305b204166
Krishna217217 <kkrishna2028@cs.sjcetpalai.ac.in>
2024-10-01 15:31:28 +0530

    Document header added

---
+++ hello python1.py
@@ -0,0 +1,17 @@
+'''
+#python program to get the student details
+#author:krishna
+#date:01-10-2024
+#version:1.0
+'''
+
+
+name=input("ENTER YOUR NAME")
+roll_number=int(input("enter the roll no;"))
+roll_number=roll_number+1
+cgpa=float(input("enter your cgpa"))
+p=cgpa+10
+print("name of the student:",name)
+print("your roll number",)
+print("marks percentage",percentage_mark,"%")
+
