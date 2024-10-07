8948db9352701844ee92e8b7c096eef3d6a6812e
Krishna217217 <kkrishna2028@cs.sjcetpalai.ac.in>
2024-10-07 11:05:17 -0700

    task 2

---
+++ krishna task 2.py
@@ -0,0 +1,9 @@
+'''
+#Write a Python program that stores a string in a variable. Extract a specific part of the string (substring) and then concatenate it with another string. Finally, display the new string.
+#author krishna
+#07-10-2024
+#version 3.12.7
+'''
+str_1="hello world!"
+str_2=str_1[6:11]
+print(str_2+"everyone")
