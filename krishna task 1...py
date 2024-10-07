b083bb4977ea0e20ad395bb12225de996156f7dd
Krishna217217 <kkrishna2028@cs.sjcetpalai.ac.in>
2024-10-07 10:55:57 -0700

    Krishna task 1.py

--- krishna task 1...py
+++ krishna task 1...py
@@ -1,3 +1,10 @@
+'''
+#Write a Python program that asks the user to input their name and age. After receiving the input, display a message that includes the user's name and age.
+#author krishna
+#07-10-2024
+#version 3.12.7
+'''
+
 name=input("enter your name")
 age=int(input("enter your age"))
 print("hello",name,"! you are",age,"years old.")
