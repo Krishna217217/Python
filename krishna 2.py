3a28b4b92ed1ae20183dd6eeff6e9549f57e863e
Krishna217217 <kkrishna2028@cs.sjcetpalai.ac.in>
2024-10-08 14:54:58 +0530

    Create, concatenate, and print a string and access a sub-string from a given string

---
+++ krishna 2.py
@@ -0,0 +1,15 @@
+'''
+#Author krishna
+#date 08-10-2024
+#version 1.0
+Simple desktop calculator using Python
+'''
+
+first_name="k"
+last_name="krishna"
+full_name=first_name+" "+last_name
+print(full_name)
+length=len(first_name)
+print(length)
+extracted_first_name=full_name[length+1:]
+print(extracted_first_name)
