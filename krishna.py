fcadb84c45dd54e00da5efd45c4996b4863fdbc4
Krishna217217 <kkrishna2028@cs.sjcetpalai.ac.in>
2024-10-08 14:26:41 +0530

    Add files via upload
    oct 8 task

---
+++ krishna.py
@@ -0,0 +1,23 @@
+'''
+#Author krishna
+#date 08-10-2024
+#version 1.0
+Simple desktop calculator using Python
+'''
+
+
+num1=int(input("enter a number"))
+num2=int(input("enter a number"))
+num3=int(input("enter a number"))
+sum=num1+num2
+print("The sum of num1 and num2 is:",sum)
+subtract=num2-num1
+print("The difference when num2 is subtracted from num1 is:",subtract)
+product=num1*num2
+print("The product of num1 and num2 is:",product)
+division=num1/num2
+print("The quotient when num1 is divided by num2 is:",division)
+modulus=num1%num2
+print("The remainder when num1 is divided by num2 is:",modulus)
+result=(num1+num2)*num3/2
+print("The result of (num1 + num2) * num3 / 2 is:",result)
