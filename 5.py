39f3b00ea1cfdb31e2037a4199ececaa7531e1a4
Krishna217217 <kkrishna2028@cs.sjcetpalai.ac.in>
2024-10-15 14:55:39 +0530

    krishnapython5

---
+++ 5.py
@@ -0,0 +1,9 @@
+'''
+python program to check whether the number divisible by 5
+'''
+number=int(input("enter your number:"))
+if number/5=0:
+    print("divisible by 5:")
+else:
+    print("not divisible by 5:")
+
