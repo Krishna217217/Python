6d63a16ea5acda6d00bf99e3c60da29e16063745
Krishna217217 <kkrishna2028@cs.sjcetpalai.ac.in>
2024-10-15 14:30:26 +0530

    krihnapython2

---
+++ krishnapython2.py
@@ -0,0 +1,7 @@
+'''
+python program to check if the number is greater than 100
+'''
+number=int(input("enter your number:"))
+if number>100:
+    print("the given number",number,"is greater than 100")
+
