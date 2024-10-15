d976b40ffc903cc2687333ea856c15e749d21578
Krishna217217 <kkrishna2028@cs.sjcetpalai.ac.in>
2024-10-15 14:38:50 +0530

    krishnapython3

---
+++ krishnapython3.py
@@ -0,0 +1,8 @@
+'''
+python program to check if a person is eligible for a driving license
+'''
+age=int(input("enter your age:"))
+if age>=16:
+    print("eligible for driving license:")
+else:
+    print("not eligible for driving license:")
