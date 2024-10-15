f4d4dd69c383043ae44e4cb0a22c8742e3e2e4b2
Krishna217217 <kkrishna2028@cs.sjcetpalai.ac.in>
2024-10-15 14:17:28 +0530

    Add files via upload
    krishna python 1

---
+++ pythonkrishna.py
@@ -0,0 +1,8 @@
+'''
+python program to check whether the given number is positive or not
+'''
+number=int(input("enter a number:"))
+if number>0:
+      print("the given number:",number,"is positive")
+else:
+      print("the given number:",number,"is negative")
