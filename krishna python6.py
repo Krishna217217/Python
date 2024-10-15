620abd2c53cc1fd11ac705d9fd42c38778e0dca9
Krishna217217 <kkrishna2028@cs.sjcetpalai.ac.in>
2024-10-15 15:10:27 +0530

    Add files via upload

---
+++ krishna python6.py
@@ -0,0 +1,14 @@
+'''
+python program to determine the larger of two numbers
+'''
+num1=int(input("enter num1:"))
+num2=int(input("enter num2:"))
+print("num1:")
+print("num2:")
+if num1>num2:
+    print("num1:")
+elif num2>num1:
+    print("num2:")
+else:
+    print("num1=num2:")
+
