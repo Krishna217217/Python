4d6570cb444ba4f58540151d2f36c00e7628b062
Krishna217217 <kkrishna2028@cs.sjcetpalai.ac.in>
2024-10-15 15:17:31 +0530

    krishnapython7

---
+++ krishnapython7.py
@@ -0,0 +1,12 @@
+'''
+python program to determine the smallest
+'''
+num1=int(input("enter num1:"))
+num2=int(input("enter num2:"))
+num3=int(input("enter num3"))
+if num1>num2&num3:
+    print("num2")
+elif num2>num1&num3:
+    print("num2")
+else:
+    print("num3")
