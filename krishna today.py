e5cff339aa4a2897819aaf750fe96c154ae1b49d
Krishna217217 <kkrishna2028@cs.sjcetpalai.ac.in>
2024-10-22 14:33:04 +0530

    Add files via upload

---
+++ krishna today.py
@@ -0,0 +1,14 @@
+'''
+phython program to convert temperature values back and forth between Celsius and Fahrenheit
+'''
+
+num1=int(input("enter a number"))
+num2=int(input("enter a number"))
+num3=int(input("enter a number"))
+if num2<num1 and num3<num1:
+    print("the largest number is:",num1)
+elif num1<num2 and num3<num2:
+    print("the largest number:",num2)
+else :
+      num1<num3 and num2<num3
+      print("the largest number:",num3)
