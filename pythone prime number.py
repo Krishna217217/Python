9263085be68e24f1eb7614a6c9f010400d617cb5
Krishna217217 <kkrishna2028@cs.sjcetpalai.ac.in>
2024-10-29 15:02:01 +0530

    Add files via upload

---
+++ pythone prime number.py
@@ -0,0 +1,11 @@
+'''
+program to check whether the given number is a prime number
+'''
+check_prime=int(input("enter a number"))
+for i in range (2,(check_prime//2)+1):
+    if check_prime%i==0:
+       break
+if i==(check_prime//2):
+    print(f"the given number {check_prime} is prime")
+else:
+    print(f"the given number {check_prime} is not a prime")
