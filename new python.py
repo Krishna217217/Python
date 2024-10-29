8235789e0169e5ca1ca00911416cad1bbe232e23
Krishna217217 <kkrishna2028@cs.sjcetpalai.ac.in>
2024-10-29 16:01:08 +0530

    Add files via upload

---
+++ new python.py
@@ -0,0 +1,12 @@
+'''
+to check the number of vowels
+'''
+string_input=input("enter a string")
+vowels_count=0
+vowels="aeiouAEIOU"
+for char in string_input:
+    if char in vowels:
+        vowels_count+=1
+print(f"number of vowels in the string {vowels_count}")
+
+
