131d902e63188e776ee51af50a5d57af18f208a5
Krishna217217 <kkrishna2028@cs.sjcetpalai.ac.in>
2024-10-29 16:14:34 +0530

    Add files via upload

--- new python.py
+++ new python.py
@@ -3,10 +3,13 @@ to check the number of vowels
 '''
 string_input=input("enter a string")
 vowels_count=0
+consonant_count=0
 vowels="aeiouAEIOU"
 for char in string_input:
     if char in vowels:
         vowels_count+=1
+    else: consonant_count+=1
 print(f"number of vowels in the string {vowels_count}")
+print(f"number of consonant in the string {consonant_count}")
 
 
